2df5970f28a53ea379161b6533477fb2f0afd748
Anbumathi Chinnasamy <105304218+Anbumathi-0212@users.noreply.github.com>
2024-10-16 22:30:01 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraformtestbucket"
-    key            = "terraform-aws-eks-workshop.tfstate"
+    key            = "terraform-aws-eks.tfstate"
     region         = "us-west-1"
     encrypt        = true
   }
